760965bb1fd01032c8078def202f30b7521ce88d
devnote-dev <devnote.dev75@gmail.com>
2023-06-14 00:51:54 +0100

    feat(application): implement get user by id

--- application/application.go
+++ application/application.go
@@ -3,9 +3,11 @@ package application
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/pteropackages/mockdactyl/exceptions"
 	"github.com/pteropackages/mockdactyl/fractal"
 )
 
@@ -14,6 +16,7 @@ func SetRoutes(router chi.Router) {
 
 	router.Route("/users", func(r chi.Router) {
 		r.Get("/", getUsers)
+		r.Get("/{id}", getUser)
 	})
 }
 
@@ -35,4 +38,18 @@ func Load(buf []byte) error {
 func getUsers(w http.ResponseWriter, r *http.Request) {
 	out, _ := fractal.SerializeList("user", store)
 	w.Write(out)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+
+	for _, u := range store {
+		if u.ID == id {
+			out, _ := fractal.SerializeItem("user", u)
+			w.Write(out)
+			return
+		}
+	}
+
+	exceptions.NotFound(w)
 }

1c1cc1045c3e8918d1793c8896d08147368b826e
devnote-dev <devnote.dev75@gmail.com>
2023-09-07 02:46:45 +0100

    feat(application): minor simplifications & cleanup

--- src/controllers/application/users.cr
+++ src/controllers/application/users.cr
@@ -1,25 +1,29 @@
 module Mockdactyl::Application
   class Users < ATH::Controller
     @[ARTA::Get("/api/application/users")]
-    def list : String
+    def list : Mockdactyl::FractalList(Mockdactyl::User)
       data = Store.users.values.map { |u| FractalItem(User).new u }
 
-      FractalList(User).new(data).to_json
+      FractalList(User).new data
     end
 
     @[ARTA::Get("/api/application/users/{id}")]
-    def index(id : Int32) : String
-      user = Store.users[id]? || raise ATH::Exceptions::NotFound.new "the requested user was not found"
+    def index(id : Int32) : Mockdactyl::FractalItem(Mockdactyl::User)
+      user = Store.users[id]? || fail_not_found!
 
-      FractalItem(User).new(user).to_json
+      FractalItem(User).new user
     end
 
     @[ARTA::Get("/api/application/users/external/{id}")]
-    def external(id : String) : String
+    def external(id : String) : Mockdactyl::FractalItem(Mockdactyl::User)
       user = Store.users.values.find { |u| u.external_id == id }
-      raise ATH::Exceptions::NotFound.new "the requested user was not found" unless user
+      fail_not_found! unless user
 
-      FractalItem(User).new(user).to_json
+      FractalItem(User).new user
+    end
+
+    private def fail_not_found! : NoReturn
+      raise ATH::Exceptions::NotFound.new "the requested user was not found"
     end
   end
 end

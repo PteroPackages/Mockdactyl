ffcab94edca383302dff3c4bb784f5d386183683
devnote-dev <devnote.dev75@gmail.com>
2023-09-07 01:26:09 +0100

    feat: add keygen endpoint for api key

--- src/mockdactyl.cr
+++ src/mockdactyl.cr
@@ -11,8 +11,17 @@ module Mockdactyl
   VERSION = "0.1.0"
 
   class App < ATH::Controller
+    @@random = Random.new
+
     get "/" do
       "Hello World"
+    end
+
+    post "/keygen" do
+      key = ApiKey.new @@random.hex(32), "auto-generated api-key"
+      Store.api_keys[key.identifier] = key
+
+      FractalItem(ApiKey).new(key).to_json
     end
   end
 end

c6b14afb21866ac10010cd81242146e4beca9e0e
devnote-dev <devnote.dev75@gmail.com>
2023-09-15 22:47:12 +0100

    fix(scripts): shebang all them scripts

--- scripts/list_users.sh
+++ scripts/list_users.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 API_KEY=$(curl http://localhost:3000/keygen -sX POST | jq -r .meta.secret_token)
 curl http://localhost:3000/api/application/users -H "Authorization: Bearer ptlc_$API_KEY"

63554ed188c6c2ca8e90e930be1508a248d1e389
devnote-dev <devnote.dev75@gmail.com>
2023-09-15 14:09:02 +0100

    feat(application): simplify routing

--- src/controllers/application/users.cr
+++ src/controllers/application/users.cr
@@ -1,20 +1,21 @@
 module Mockdactyl::Application
+  @[ARTA::Route(path: "/api/application/users")]
   class Users < ATH::Controller
-    @[ARTA::Get("/api/application/users")]
+    @[ARTA::Get("")]
     def list : Mockdactyl::FractalList(Mockdactyl::User)
       data = Store.users.values.map { |u| FractalItem(User).new u }
 
       FractalList(User).new data
     end
 
-    @[ARTA::Get("/api/application/users/{id}")]
+    @[ARTA::Get("{id}")]
     def index(id : Int32) : Mockdactyl::FractalItem(Mockdactyl::User)
       user = Store.users[id]? || fail_not_found!
 
       FractalItem(User).new user
     end
 
-    @[ARTA::Get("/api/application/users/external/{id}")]
+    @[ARTA::Get("external/{id}")]
     def external(id : String) : Mockdactyl::FractalItem(Mockdactyl::User)
       user = Store.users.values.find { |u| u.external_id == id }
       fail_not_found! unless user
